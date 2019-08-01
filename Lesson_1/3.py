4868d0a18c6c10589b8aa5a8390570c986de565c
lokasan <pololopo@yandex.ru>
2019-08-01 14:46:59 +0300

    complete all

--- Lesson_1/3.py
+++ Lesson_1/3.py
@@ -1,2 +1,21 @@
 # 3.	По введенным пользователем координатам двух точек вывести
 # уравнение прямой вида y = kx + b, проходящей через эти точки.
+x1 = int(input('Введите x1: '))
+y1 = int(input('Введите y1: '))
+x2 = int(input('Введите x2: '))
+y2 = int(input('Введите y2: '))
+
+if x2 - x1 == 0 or y2 - y1 == 0:
+    exit()
+else:
+    x = y1 - y2
+    y = x2 - x1
+    b = x1 * y2 - x2 * y1
+    if y > 0 and b > 0:
+        print(f'{x}x + {y}y + {b} = 0')
+    elif y < 0 and b < 0:
+        print(f'{x}x {y}y {b} = 0')
+    elif y < 0 and b > 0:
+        print(f'{x}x {y}y + {b} = 0')
+    elif y > 0 and b < 0:
+        print(f'{x}x + {y}y {b} = 0')
--- Lesson_1/6.py
+++ Lesson_1/6.py
@@ -1,1 +1,7 @@
 # 6.	Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+number = int(input('Введите порядкрвый номер буквы английского алфавита: '))
+if number < 1 or number > 26:
+    exit(-1)
+else:
+    unicode_first_symb = 96
+    print(chr(unicode_first_symb + number))
