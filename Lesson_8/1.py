7449c8f51f2bfaa480f651cb1a96815906a53852
lokasan <pololopo@yandex.ru>
2019-08-25 18:51:10 +0300

    lesson8

--- Lesson_8/1.py
+++ Lesson_8/1.py
@@ -1,3 +1,42 @@
 """
 1. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
 """
+from collections import Counter, deque
+
+
+code_table = dict()
+
+
+def haffman_code(tree, path=''):
+    if not isinstance(tree, dict):
+        code_table[tree] = path
+    else:
+        haffman_code(tree[0], path=f'{path}0')
+        haffman_code(tree[1], path=f'{path}1')
+
+
+string = list('bill piu will')
+freq = Counter(string)
+freq = deque(sorted(freq.items(), key=lambda symbol: symbol[1]))
+
+if len(freq) != 1:
+    while len(freq) > 1:
+        wt = freq[0][1] + freq[1][1]
+        save = {0: freq.popleft()[0],
+                1: freq.popleft()[0]}
+        for i, c in enumerate(freq):
+            if wt > c[1]:
+                continue
+            else:
+                freq.insert(i, (save, wt))
+                break
+        else:
+            freq.append((save, wt))
+else:
+    wt = freq[0][1]
+    save = {0: freq.popleft()[0], 1: None}
+    freq.append((save, wt))
+
+haffman_code(freq[0][0])
+for i in string:
+    print(code_table[i], end=" ")
--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,16 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+import hashlib
+string = input("Введите строку только из маленьких латинских букв: ")
+count = len(string)
+save = set()
+for i in range(count):
+    if i == 0:
+        count = len(string) - 1
+    else:
+        count = len(string)
+    for j in range(count, i, - 1):
+        save.add(hashlib.sha1(string[i:j].encode('utf8')).hexdigest())
+print(save)
+print(f'Количество подстрок в строке "{string}" = {len(save)}')
